d17cf877edccb7ec89d6a78140aafb0a6adfa0be
wooldridge <mike@hellion.org>
2023-02-22 00:53:19 -0800

    Add example application

---
+++ examples/ml-demo-app/src/App.js
@@ -0,0 +1,13 @@
+import { Test } from 'ml-application-framework';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Testing the app</p>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
