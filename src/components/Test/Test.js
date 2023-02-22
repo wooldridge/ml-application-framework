e1d9ebd97deb6032682460afdd8a4b7ceff4c4b3
wooldridge <mike@hellion.org>
2023-02-22 00:48:12 -0800

    Initial commit

---
+++ src/components/Test/Test.js
@@ -0,0 +1,10 @@
+import './Test.scss';
+
+const Test = () => {
+  console.log('Test console.log');
+  return (
+    <div className="test-text">Test in a div</div>
+  );
+}
+
+export default Test;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx"]
+        },
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-syntax-jsx"]
+          }
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+      new CleanWebpackPlugin()
+  ]
+};
